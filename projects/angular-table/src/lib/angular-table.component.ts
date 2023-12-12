124ddf75c0a545cb1140564254f64588e201fd2a
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2023-12-12 12:53:42 +0100

    init lib

---
+++ projects/angular-table/src/lib/angular-table.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-angular-table',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      angular-table works!
+    </p>
+  `,
+  styles: ``
+})
+export class AngularTableComponent {
+
+}
---
+++ projects/angular-table/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of angular-table
+ */
+
+export * from './lib/angular-table.service';
+export * from './lib/angular-table.component';
